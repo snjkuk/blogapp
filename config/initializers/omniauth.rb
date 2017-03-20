af42c88df9455a90b988f0fc0cffa65f49da0e81
Ravikant Rajak <er.ravi.rajak2790@gmail.com>
2017-03-20 11:50:12 +0530

    blog

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-     provider :google_oauth2, "313930346927-rid262bt0recr7llcf7oo479dbcsg19d.apps.googleusercontent.com", "FVoFbH8dswFNXM-4E6Jsxo15"
+      provider :google_oauth2, "76962942646-6f9gq2qd50obiacqo3pon6vr0gul0l38.apps.googleusercontent.com", "cXCibofu8WmROBGxxpfBhrCL"
+     # provider :google_oauth2, "109313246351-hd9l5gt2d4u3mcpuj2r49cgv11dj1av7.apps.googleusercontent.com", "emyojGz9bZleTL4-FetbgXA5"
   #   scope: ['https://mail.google.com/','contacts','plus.login','plus.me','userinfo.email','userinfo.profile']
      provider :twitter, "37FRvK516zX4PiWGzXzRMja0J", "5lgd0wWmDAm9KoNyxV8jVuiDDCrbWMmpVt3UPLK583V4ARcHWA"
      provider :facebook, "1857150984499762", "c9b581e5290d3b5c0a0268ded29f4664"
